cf8553abb775acc47ee1abcfdeac405222295cad
ethantrainshard <52568986+ethantrainshard@users.noreply.github.com>
2020-09-17 05:42:29 -0400

    Initial upload

---
+++ fuzzer.py
@@ -0,0 +1,26 @@
+import socket, time, sys
+
+ip = "10.10.167.218"                # Change the IP
+port = 1337                         # Change the port number
+timeout = 5
+
+buffer = []
+counter = 100
+while len(buffer) < 30:
+    buffer.append("A" * counter)
+    counter += 100
+
+for string in buffer:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        connect = s.connect((ip, port))
+        s.recv(1024)
+        print("Fuzzing with %s bytes" % len(string))
+        s.send("OVERFLOW1 " + string + "\r\n")          # Change the prefix as accordingly
+        s.recv(1024)
+        s.close()
+    except:
+        print("Could not connect to " + ip + ":" + str(port))
+        sys.exit(0)
+    time.sleep(1)
